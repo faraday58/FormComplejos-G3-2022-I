962f786c562c515beec7ab0d43262c8c60dd36a9
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-09-27 12:51:43 -0500

    Complejos en consola

--- ConsolaComplejos/Program.cs
+++ ConsolaComplejos/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
-
+using Complejos;
 namespace ConsolaComplejos
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Complejo c1 = new Complejo(2, 4);
+            Complejo c2 = new Complejo(4, 3);
+
+            Complejo c3 = c1 + c2;
+
+            Console.WriteLine("{0} + {1} = {2} ",c1,c2,c3);
+            Console.ReadLine();
+
         }
     }
 }
