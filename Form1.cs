6e0736a6a03cf21bba2e26612a1aa82f5c3616bd
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-09-29 12:17:59 -0500

    Formulario complejos

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,10 @@ namespace FormComplejos_G3_2022_I
 
         private void btnSumar_Click(object sender, EventArgs e)
         {
-            
+            Complejo c1 = new Complejo(float.Parse(txtbRealComplejo1.Text), float.Parse( txtbImaginariaComplejo1.Text));
+            Complejo c2 = new Complejo(float.Parse(txtbRealComplejo2.Text), float.Parse(txtbImaginarioComplejo2.Text));
+            Complejo c3 = c1 + c2;
+            lbResultadoComplejo1.Text = c3.ToString();
 
         }
     }
